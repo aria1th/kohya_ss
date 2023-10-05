8b6d0342bde39254f8fca2e68d80c953f596bca6
aria1th <35677394+aria1th@users.noreply.github.com>
2023-10-06 03:36:05 +0900

    I think this is far more intuitive

--- library/webui_utils.py
+++ library/webui_utils.py
@@ -12,7 +12,7 @@ from accelerate import Accelerator
 from threading import Thread
 from concurrent.futures import ThreadPoolExecutor
 from webuiapi.webuiapi import WebUIApi, ControlNetUnit, QueuedTaskResult
-from webuiapi.test_utils import InstanceHolder, open_controlnet_image
+from webuiapi.test_utils import InstanceHolder, open_controlnet_image, open_mask_image, raw_b64_img
 
 executor_thread_pool = ThreadPoolExecutor(max_workers=1) # sequential execution
     
@@ -130,8 +130,15 @@ def request_sample(
     ):
     """
     Generate sample with external webui. This function is thread-locking. 
+    Prompt file should be a json file that can be parsed for webuiapi.
+    real 'prompts' and 'negative prompt' should contain regex_to_replace, which will be replaced with ckpt_name.
+    
+    example prompt:
+        "1girl, <lora:$1>"
+    with example regex_to_replace $1
+    will be converted to
+        "1girl, <lora:ckpt_name>"
     """
-    instance_holder = InstanceHolder(webui_instance)
     if prompt_file_path.endswith(".json"):
         with open(prompt_file_path, "r", encoding="utf-8") as f:
             prompts = json.load(f)
@@ -139,60 +146,11 @@ def request_sample(
         prompts = [prompts] # convert to list if not list
     os.makedirs(output_dir_path, exist_ok=True)
     for i, prompt in enumerate(prompts):
-        # check if prompt is a dict
-        if isinstance(prompt, dict):
-            negative_prompt = prompt.get("negative_prompt", None)
-            sample_steps = prompt.get("sample_steps", 30)
-            width = prompt.get("width", 512)
-            height = prompt.get("height", 512)
-            scale = prompt.get("scale", 7.5)
-            seed = prompt.get("seed", -1)
-            controlnet_image = prompt.get("controlnet_image", None)
-            rp_mode = prompt.get("rp_mode", None)
-            positive_prompt = prompt.get("prompt", None)
-            checkpoint_name = prompt.get("checkpoint_name", None)
-            enable_hr = prompt.get("enable_hr", False)
-            hr_scale = prompt.get("hr_scale", 1.0)
-            hr_model = prompt.get("hr_model", 'R-ESRGAN 4x+ Anime6B')
-            hr_noise = prompt.get("hr_noise", 0.5)
-            rp_mode_params = None
-            if rp_mode is not None:
-                # get rp_mode params
-                prompt_1_arg: str = prompt.get("prompt_1_arg", "") # necessary
-                prompt_2_arg: str = prompt.get("prompt_2_arg", "") # necessary
-                attach_lora_to_which: int = prompt.get("attach_lora_to_which", 1)
-                lora_stop_steps: Tuple[int, int] = prompt.get("lora_stop_steps", (0, 0))
-                extra_args: dict = prompt.get("extra_args", {})
-                controlnet_path: str = prompt.get("controlnet_path", "")
-                controlnet_preprocessor: str = prompt.get("controlnet_preprocessor", "none") 
-                controlnet_model: str = prompt.get("controlnet_model", "none")
-                base_prompt: str = prompt.get("base_prompt", "") 
-                common_prompt: str = prompt.get("common_prompt", "") 
-                negative_prompt: str = prompt.get("negative_prompt", "")
-                regional_prompter_args: dict | None = prompt.get("regional_prompter_args", None)
-                # check if necessary params are present
-                if prompt_1_arg == "" or prompt_2_arg == "":
-                    print(f"Invalid prompt format. Must include prompt_1_arg, prompt_2_arg, controlnet_path, controlnet_model when using rp_mode, got {prompt}")
-                    continue
-                # wrap as dict
-                rp_mode_params = {
-                    "prompt_1_arg": prompt_1_arg + f"<lora:{ckpt_name}:1>" if attach_lora_to_which == 0 else prompt_1_arg,
-                    "prompt_2_arg": prompt_2_arg + f"<lora:{ckpt_name}:1>" if attach_lora_to_which == 1 else prompt_2_arg,
-                    "lora_stop_steps": lora_stop_steps,
-                    "extra_args": extra_args,
-                    "controlnet_path": controlnet_path,
-                    "controlnet_preprocessor": controlnet_preprocessor,
-                    "controlnet_model": controlnet_model,
-                    "base_prompt": base_prompt,
-                    "common_prompt": common_prompt,
-                    "negative_prompt": negative_prompt,
-                    "regional_prompter_args": regional_prompter_args,
-                }
-                # reconstruct positive_prompt 
-                positive_prompt = f"{common_prompt} {prompt_1_arg} BREAK {common_prompt} {prompt_2_arg}"
-        else:
+        if not isinstance(prompt, dict):
             print(f"Invalid prompt format. Must be a dict, got {prompt}")
             continue
+        # controlnet params
+        controlnet_params = ["controlnet_model", "controlnet_image", "controlnet_preprocessor"]
         # check controlnet params
         if prompt.get("controlnet_model", None) is not None:
             controlnet_model = prompt["controlnet_model"]
@@ -208,51 +166,48 @@ def request_sample(
             controlnet_units = [controlnet_unit]
         else:
             controlnet_units = []
-        
-        queued_task_result = None
-        if rp_mode is None: # basic test
-            queued_task_result = instance_holder.instance.txt2img_task(
-                controlnet_units=controlnet_units,
-                prompt=positive_prompt,
-                negative_prompt=negative_prompt,
-                width=width,
-                height=height,
-                cfg_scale=scale,
-                seed=seed,
-                steps=sample_steps,
-                checkpoint_name=checkpoint_name, # can be None
-                enable_hr=enable_hr,
-                hr_scale=hr_scale,
-                hr_noise=hr_noise,
-                hr_upscaler=hr_model,
-            )
-        elif rp_mode == "division": # divide mode
-            queued_task_result = instance_holder.test_division(
-                controlnet_model=controlnet_model,
-                controlnet_path=controlnet_image,
-                controlnet_preprocessor=controlnet_preprocessor,
-                **rp_mode_params
-            )
-        elif rp_mode == 'mask':
-            queued_task_result = instance_holder.test_mask(
-                controlnet_model=controlnet_model,
-                controlnet_path=controlnet_image,
-                controlnet_preprocessor=controlnet_preprocessor,
-                **rp_mode_params
-            )
-        else:
-            print(f"Invalid rp_mode. Must be division or mask, got {rp_mode}")
+            
+        if "regex_to_replace" not in prompt:
+            print(f"Invalid prompt format. Must include regex_to_replace, got {prompt}")
             continue
+        # replace regex_to_replace with ckpt_name, search in prompt and negative prompt
+        prompt["prompt"] = prompt["prompt"].replace(prompt["regex_to_replace"], ckpt_name)
+        prompt["negative_prompt"] = prompt["negative_prompt"].replace(prompt["regex_to_replace"], ckpt_name)
+        # pop regex_to_replace
+        prompt.pop("regex_to_replace")
+        # pop controlnet params
+        for param in controlnet_params:
+            if param in prompt:
+                prompt.pop(param)
+        # if seed is not specified, generate random seed
+        if "seed" not in prompt or prompt["seed"] is -1:
+            prompt["seed"] = np.random.randint(0, 100000)
+        # fix alwayson_script args if there is any 'file' references as strings
+        if 'alwayson_scripts' in prompt:
+            alwayson_scripts_dict = prompt['alwayson_scripts']
+            for key, value in alwayson_scripts_dict.items():
+                if isinstance(value, str) and os.path.isfile(value):
+                    # convert to base64
+                    base64_str = raw_b64_img(open_mask_image(value))
+                    alwayson_scripts_dict[key] = base64_str
+                    print(f"Converted {value} to base64 string")
+        positive_prompt, negative_prompt, seed = prompt.get("positive_prompt", "positive:None"), prompt.get("negative_prompt", "negative:None"), prompt.get("seed", 0)
+        queued_task_result = webui_instance.txt2img_task(
+            controlnet_units=controlnet_units,
+            **prompt
+        )
         # start thread to wait for result
         def wait_and_save(queued_task_result:QueuedTaskResult, output_dir_path, output_name, accelerator):
-            while not queued_task_result.check_finished():
+            while not queued_task_result.check_finished(): # can throw exception if webui is not reachable or broken
                 time.sleep(1)
+            # 6 digits of time
+            strftime = f"{time.strftime('%Y%m%d_%H%M%S')}"
             result = queued_task_result.get_image()
             if result is None:
                 print("Failed to generate sample")
                 return
             image = result.image
-            image.save(os.path.join(output_dir_path, f"{output_name}_{i}.png"))
+            image.save(os.path.join(output_dir_path, f"{output_name}_{strftime}_{i}.png"))
             log_wandb(accelerator, image, positive_prompt, negative_prompt, seed)
         # start thread
         executor_thread_pool.submit(wait_and_save, queued_task_result, output_dir_path, output_name, accelerator)
