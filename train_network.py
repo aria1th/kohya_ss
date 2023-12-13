c03179810f38ec453d1d8c4748f5124ba61ed402
AngelBottomless <aria1th@naver.com>
2023-12-13 19:20:59 +0900

    skip encode when disabled

--- train_network.py
+++ train_network.py
@@ -845,14 +845,17 @@ class NetworkTrainer:
                                 args.max_token_length // 75 if args.max_token_length else 1,
                                 clip_skip=args.clip_skip,
                             )
-                            text_encoder_conds_b = get_weighted_text_embeddings(
-                                tokenizer,
-                                text_encoder,
-                                batch["captions_without_cls"],
-                                accelerator.device,
-                                args.max_token_length // 75 if args.max_token_length else 1,
-                                clip_skip=args.clip_skip,
-                            )
+                            if args.contrastive_class_learning:
+                                text_encoder_conds_b = get_weighted_text_embeddings(
+                                    tokenizer,
+                                    text_encoder,
+                                    batch["captions_without_cls"],
+                                    accelerator.device,
+                                    args.max_token_length // 75 if args.max_token_length else 1,
+                                    clip_skip=args.clip_skip,
+                                )
+                            else:
+                                text_encoder_conds_b = None
                         else:
                             text_encoder_conds, text_encoder_conds_b = self.get_text_cond(
                                 args, accelerator, batch, tokenizers, text_encoders, weight_dtype
